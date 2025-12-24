488cb98541fa9082f5ab156fe3b18962054bd580
ksenia topchu <pureshka7@gmail.com>
2025-12-24 12:13:37 +0200

    добавление функции вывода массива

--- Source.c
+++ Source.c
@@ -5,17 +5,16 @@
 #include<stdlib.h>
 #define n 5
 void fill(int*, int);
+void print(int*, int);
 void main()
 {
 	setlocale(LC_ALL, "rus");
 	int a[n], i;
 	float s = 0;
 	fill(a, n);
+	print(a, n);
 	for (i = 0;i < n;i++)
-	{
 		s += a[i];
-		printf("%i \t", a[i]);
-	}
 	printf("\nсреднее арифметическое: %.2f", s / n);
 }
 void fill(int* ptr, int num)
@@ -23,4 +22,9 @@ void fill(int* ptr, int num)
 	srand(time(NULL));
 	for (int i = 0;i < num;i++)
 		ptr[i] = rand() % 10;
+}
+void print(int* ptr, int num)
+{
+	for (int i = 0;i < num;i++)
+		printf("%4i", ptr[i]);
 }
