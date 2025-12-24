a9fa3815b5f797e48e242771e2b1d78766c89a7f
ksenia topchu <pureshka7@gmail.com>
2025-12-24 11:24:55 +0200

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+#include<time.h>
+#include<stdlib.h>
+#define n 5
+void main()
+{
+	int a[n], i;
+	float s = 0;
+	srand(time(NULL));
+	for (i = 0;i < n;i++)
+	{
+		a[i] = rand() % 11;
+		s += a[i];
+		printf("%i \t", a[i]);
+	}
+	printf("\n%.2f", s / n);
+}
